8335dfa645b44315114767000c04333873e6018f
Daniel Pietzsch <daniel@visfleet.com>
2012-03-27 16:46:24 +1300

    making the ID of a step accessible
    this fixes a bug where existing steps where re-added to the job instead of updated

--- lib/vworkapp_ruby/step.rb
+++ lib/vworkapp_ruby/step.rb
@@ -1,6 +1,6 @@
 module VWorkApp
   class Step < Base
-    hattr_accessor :name, {:location => VWorkApp::Location}, :completed_at
+    hattr_accessor :id, :name, {:location => VWorkApp::Location}, :completed_at
     self.include_root_in_json = false
 
     def ==(other)
