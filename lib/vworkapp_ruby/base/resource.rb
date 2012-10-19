c3aaef520615a3ea14b6255c90105805964e747d
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2012-10-19 14:44:39 +1300

    Reverting id removal from PUT data. Can now be handled ok by the API

--- lib/vworkapp_ruby/base/resource.rb
+++ lib/vworkapp_ruby/base/resource.rb
@@ -53,7 +53,7 @@ module VWorkApp
 
     def update(options = {})
       validate_and_raise
-      perform(:put, "/#{resource_name.pluralize}/#{id}.xml", options, self.to_xml(:except => [:id]))
+      perform(:put, "/#{resource_name.pluralize}/#{id}.xml", options, self.to_xml)
     end
     
     def delete(options = {})
